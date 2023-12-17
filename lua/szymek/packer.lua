240351e9b4e5ef8b0e987cb07e4dc250ea69eae3
Szymon Papuga <papuga.szymon@gmail.com>
2023-12-17 14:54:57 +0100

    remove harpoon and add oldfiles picker telescope keymap

--- lua/szymek/packer.lua
+++ lua/szymek/packer.lua
@@ -24,7 +24,6 @@ return require('packer').startup(function(use)
     use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
     use('nvim-treesitter/playground')
     use('nvim-lua/plenary.nvim')
-    use('ThePrimeagen/harpoon')
     use('mbbill/undotree')
     use('tpope/vim-fugitive')
 
