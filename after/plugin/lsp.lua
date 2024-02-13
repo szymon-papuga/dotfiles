a666ad5b20de82032d964624f5a988b792220a16
Szymon Papuga <papuga.szymon@gmail.com>
2024-02-13 18:55:18 +0100

    prisma language server

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -19,7 +19,7 @@ end)
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-    ensure_installed = { 'tsserver', 'rust_analyzer', 'lua_ls' },
+    ensure_installed = { 'tsserver', 'rust_analyzer', 'lua_ls', 'prismals' },
     handlers = {
         lsp_zero.default_setup,
         lua_ls = function()
