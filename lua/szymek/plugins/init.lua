87841d9815c4397ce1f92c8cd7599f59036f602d
Szymon Papuga <papuga.szymon@gmail.com>
2024-05-03 10:47:36 +0200

    colorscheme

--- lua/szymek/plugins/init.lua
+++ lua/szymek/plugins/init.lua
@@ -1,4 +1,10 @@
 return {
   "nvim-lua/plenary.nvim", -- lua functions that many plugins use
   "christoomey/vim-tmux-navigator", -- tmux & split window navigation
+  {
+    "rose-pine/neovim", name = "rose-pine",
+    config = function()
+      vim.cmd.colorscheme("rose-pine")
+    end
+  }
 }
