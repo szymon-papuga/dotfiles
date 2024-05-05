ae776b43f0d992638ad0d868d55afd57bb69f0b8
Szymon Papuga <papuga.szymon@gmail.com>
2024-05-05 08:27:50 +0200

    disable lazy.nvim change detection

--- lua/szymek/lazy.lua
+++ lua/szymek/lazy.lua
@@ -11,4 +11,8 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("szymek.plugins")
+require("lazy").setup("szymek.plugins", {
+  change_detection = {
+    notify = false,
+  },
+})
