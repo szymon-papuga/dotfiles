98c8bd5e71a19b9e69e8aebf84ace506354373bf
Szymon Papuga <papuga.szymon@gmail.com>
2024-05-10 20:01:27 +0200

    dressing

--- lua/szymek/plugins/init.lua
+++ lua/szymek/plugins/init.lua
@@ -1,17 +1,22 @@
 return {
-  "nvim-lua/plenary.nvim", -- lua functions that many plugins use
-  "christoomey/vim-tmux-navigator", -- tmux & split window navigation
-  {
-    "rose-pine/neovim", name = "rose-pine",
-    config = function()
-      vim.cmd.colorscheme("rose-pine")
-    end
-  },
-  {
-    "numToStr/Comment.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-      require('Comment').setup()
-    end
-  }
+	"nvim-lua/plenary.nvim", -- lua functions that many plugins use
+	"christoomey/vim-tmux-navigator", -- tmux & split window navigation
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		config = function()
+			vim.cmd.colorscheme("rose-pine")
+		end,
+	},
+	{
+		"numToStr/Comment.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			require("Comment").setup()
+		end,
+	},
+	{
+		"stevearc/dressing.nvim",
+		event = "VeryLazy",
+	},
 }
